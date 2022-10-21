c04cb0c9e0e6b672becdcbc3c661b78acd9c5de5
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-10-21 11:55:22 -0300

    Exemplo Final - Login SessionStorage

--- pf0670/login/src/components/Login.jsx
+++ pf0670/login/src/components/Login.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
+  const [usuario, setUsuario] = useState({
+    login: "",
+    senha: "",
+  });
+
+  const handleChange = (e) => {
+    setUsuario({ ...usuario, [e.target.name]: e.target.value });
+  };
+
+  const logar = async (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(usuario),
+    };
+
+    const response = await fetch(
+      "http://localhost:8080/ControleAcesso/rest/login/",
+      requestOptions
+    );
+
+    const data = await response.json();
+
+    if (data.login) {
+      sessionStorage.setItem("usuario-validado", data.login);
+      window.location = "/home";
+    } else {
+      window.location = "/";
+    }
+  };
+
   return (
     <div>
       <h2>LOGIN DE USUÁRIOS</h2>
       <div>
-        <form onSubmit="">
+        <form onSubmit={logar}>
           <fieldset>
             <legend>LOGIN</legend>
             <div>
@@ -15,8 +49,8 @@ export default function Login() {
                 name="login"
                 id="idLogin"
                 placeholder="Login"
-                value=""
-                onChange=""
+                value={usuario.login}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -26,8 +60,8 @@ export default function Login() {
                 name="senha"
                 id="idSenha"
                 placeholder="Senha"
-                value=""
-                onChange=""
+                value={usuario.senha}
+                onChange={handleChange}
               />
             </div>
             <div>
--- pf0670/login/src/components/Produto.jsx
+++ pf0670/login/src/components/Produto.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
+
+const verificar = sessionStorage.getItem("usuario-validado");
 
 export default function Produto() {
+  useEffect(() => {
+    if (verificar == null) {
+      window.location = "/";
+    }
+  }, []);
+
   return <div>Produto</div>;
 }
